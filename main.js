82e75c2233f041ed5460f7fb45cd62ee396b4a87
Udhay <udaymahajan97@gmail.com>
2021-05-28 14:05:22 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            product: 'Socks',
+            image: 'assets/images/socks_green.jpg',
+            inStock: 8,
+            details: ["50% Cotton", "50% Polyester"]
+        }
+    }
+})
